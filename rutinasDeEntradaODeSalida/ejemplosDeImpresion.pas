56b04ac31b3735d93e6f479e8175dd3ff4137a06
Developer LuisF3 <developerluisfernando3@gmail.com>
2020-06-22 22:39:43 -0500

    [ADD, código ejemplos de impresión]

---
+++ rutinasDeEntradaODeSalida/ejemplosDeImpresion.pas
@@ -0,0 +1,13 @@
+Program EjemplosDeImpresion;
+uses crt;
+Var
+        A : Integer;
+Begin
+        clrscr;
+        A := 10;
+        Write ('Hola');
+        Write (6 * 5);
+        Write (A);
+        Write ('Resultado: ', 2 * A);
+        readkey;
+End.
---
+++ rutinasDeEntradaODeSalida/ejemplosDeImpresion_II.pas
@@ -0,0 +1,13 @@
+Program EjemplosDeImpresion_II;
+uses crt;
+Var
+        A : Integer;
+Begin
+        clrscr;
+        A := 10;
+        WriteLn ('Hola');
+        WriteLn (6 * 5);
+        WriteLn (A);
+        WriteLn ('Resultado: ', 2 * A);
+        readkey;
+End.
