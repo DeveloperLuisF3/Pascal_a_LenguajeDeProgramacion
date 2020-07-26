0084c9426b0ae603ba18fad9faaaa51b8f33fcf2
Developer LuisF3 <developerluisfernando3@gmail.com>
2020-07-26 17:03:13 -0500

    [ADD, códigoSelecciónCompleta-secciónIF-THEN-ELSE]

---
+++ estructurasDeProgramacion/seleccionCompleta.pas
@@ -0,0 +1,17 @@
+Program SeleccionSimple;
+{ Este programa hace una divisi¢n, pero verifica que el divisor sea diferente de cero }
+uses crt;
+Var
+        Dividendo, Divisor, Cociente : Real;
+Begin
+        clrscr;
+        Read (DIVIDENDO, DIVISOR);
+        If DIVISOR = 0 Then
+                Write ('No Se Puede Hacer La Divisi¢n')
+        Else
+                Begin
+                        COCIENTE := DIVIDENDO / DIVISOR;
+                        Write (Cociente);
+                End;
+        readkey;
+End.
